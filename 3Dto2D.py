f13576fea8988c958b7edd33978951a6045db47f
rh2740 <rh2740@nyu.edu>
2019-03-26 15:50:40 -0400

    03.26.1

---
+++ 3Dto2D.py
@@ -0,0 +1,29 @@
+import numpy as np
+import skimage as ski
+import os
+from PIL import Image
+
+
+# Get all images in the root directory
+def image_ids_in(root_dir, ignore=['.DS_Store', 'dict.csv']):
+    ids = []
+    for id in os.listdir(root_dir):
+        if id in ignore:
+            print('Skipping ID:', id)
+        else:
+            print(id.split('.t')[0])
+            ids.append(id.split('.t')[0])
+    return ids
+
+images = image_ids_in('Images/')
+
+for i in images:
+    im = ski.io.imread('Images/{}.tif'.format(i))
+    os.mkdir('Images/{}'.format(i))
+    for a in range(np.shape(im)[0]):
+        print(a)
+        sgim = im[a,:,:,:]
+        ski.io.imsave('Images/{}/{}_{}.tif'.format(i, str(a), i), sgim)
+        os.rename('Images/{}/{}_{}.tif'.format(i, str(a), i), 'Images/{}/{}_{}.png'.format(i, str(a), i))
+print('done!')
+
