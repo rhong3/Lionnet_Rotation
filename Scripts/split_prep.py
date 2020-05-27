ac96faabecc4ee5bc4adc2af2bb16a5c0e26c47b
HONGRUNYU <michaelhong1995@gmail.com>
2020-05-27 13:36:07 -0400

    5.27.4

--- Scripts/split_prep.py
+++ Scripts/split_prep.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import skimage.morphology as mph
 import pandas as pd
+from scipy.misc import imsave
 
 sum = pd.read_csv("../stage_1_train/summary.csv",
                   usecols=["image_id", "width", "height", "total_masks", "hsv_dominant", "hsv_cluster"])
@@ -27,8 +28,13 @@ for i in range(1, 8):
         except FileExistsError:
             pass
 
-        io.imsave('../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/images/C1-FB323A_CSC_Rd1_{}_{}_pad.png'.
-                  format(str(i), str(j+1), str(i), str(j+1)), image[j, :, :])
+        img = np.array(np.zeros([1024, 1024, 3]))
+        img[:, :, 0] = image[j, :, :]
+        img[:, :, 1] = image[j, :, :]
+        img[:, :, 2] = image[j, :, :]
+
+        imsave('../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/images/C1-FB323A_CSC_Rd1_{}_{}_pad.png'.
+                  format(str(i), str(j+1), str(i), str(j+1)), img)
         io.imsave('../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/label/Combined_pad.png'.
                   format(str(i), str(j+1), str(i), str(j+1)), label[j, :, :])
 
