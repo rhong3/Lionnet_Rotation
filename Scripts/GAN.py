b095f028d139fac4d31366d23b75f87c5b1f2038
HONGRUNYU <michaelhong1995@gmail.com>
2020-07-13 12:22:04 -0400

    7.13.1

---
+++ Scripts/GAN.py
@@ -0,0 +1,221 @@
+# Cycle-GAN 3D models
+
+import random
+import time
+import datetime
+import sys
+from torch.autograd import Variable
+import torch
+from visdom import Visdom
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        conv_block = [  nn.ReplicationPad3d(1),
+                        nn.Conv3d(in_features, in_features, 3),
+                        nn.InstanceNorm3d(in_features),
+                        nn.ReLU(inplace=True),
+                        nn.ReplicationPad3d(1),
+                        nn.Conv3d(in_features, in_features, 3),
+                        nn.InstanceNorm3d(in_features)  ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, input_nc, output_nc, n_residual_blocks=9):
+        super(Generator, self).__init__()
+
+        # Initial convolution block
+        model = [   nn.ReplicationPad3d(3),
+                    nn.Conv3d(input_nc, 64, 7),
+                    nn.InstanceNorm3d(64),
+                    nn.ReLU(inplace=True) ]
+
+        # Downsampling
+        in_features = 64
+        out_features = in_features*2
+        for _ in range(2):
+            model += [  nn.Conv3d(in_features, out_features, 3, stride=2, padding=1),
+                        nn.InstanceNorm3d(out_features),
+                        nn.ReLU(inplace=True) ]
+            in_features = out_features
+            out_features = in_features*2
+
+        # Residual blocks
+        for _ in range(n_residual_blocks):
+            model += [ResidualBlock(in_features)]
+
+        # Upsampling
+        out_features = in_features//2
+        for _ in range(2):
+            model += [  nn.ConvTranspose3d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
+                        nn.InstanceNorm3d(out_features),
+                        nn.ReLU(inplace=True) ]
+            in_features = out_features
+            out_features = in_features//2
+
+        # Output layer
+        model += [  nn.ReplicationPad3d(3),
+                    nn.Conv3d(64, output_nc, 7),
+                    nn.Tanh() ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
+
+class Discriminator(nn.Module):
+    def __init__(self, input_nc):
+        super(Discriminator, self).__init__()
+
+        # A bunch of convolutions one after another
+        model = [   nn.Conv3d(input_nc, 64, 4, stride=2, padding=1),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv3d(64, 128, 4, stride=2, padding=1),
+                    nn.InstanceNorm3d(128),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv3d(128, 256, 4, stride=2, padding=1),
+                    nn.InstanceNorm3d(256),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv3d(256, 512, 4, padding=1),
+                    nn.InstanceNorm3d(512),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        # FCN classification layer
+        model += [nn.Conv3d(512, 1, 4, padding=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x =  self.model(x)
+        # Average pooling and flatten
+        return F.avg_pool3d(x, x.size()[2:]).view(x.size()[0], -1)
+
+
+def tensor2image(tensor):
+    image = 127.5 * (tensor[0].cpu().float().numpy() + 1.0)
+    if image.shape[0] == 1:
+        image = np.tile(image, (3, 1, 1))
+    return image.astype(np.uint8)
+
+
+class Logger():
+    def __init__(self, n_epochs, batches_epoch):
+        self.viz = Visdom()
+        self.n_epochs = n_epochs
+        self.batches_epoch = batches_epoch
+        self.epoch = 1
+        self.batch = 1
+        self.prev_time = time.time()
+        self.mean_period = 0
+        self.losses = {}
+        self.loss_windows = {}
+        self.image_windows = {}
+
+    def log(self, losses=None, images=None):
+        self.mean_period += (time.time() - self.prev_time)
+        self.prev_time = time.time()
+
+        sys.stdout.write(
+            '\rEpoch %03d/%03d [%04d/%04d] -- ' % (self.epoch, self.n_epochs, self.batch, self.batches_epoch))
+
+        for i, loss_name in enumerate(losses.keys()):
+            if loss_name not in self.losses:
+                self.losses[loss_name] = losses[loss_name].data[0]
+            else:
+                self.losses[loss_name] += losses[loss_name].data[0]
+
+            if (i + 1) == len(losses.keys()):
+                sys.stdout.write('%s: %.4f -- ' % (loss_name, self.losses[loss_name] / self.batch))
+            else:
+                sys.stdout.write('%s: %.4f | ' % (loss_name, self.losses[loss_name] / self.batch))
+
+        batches_done = self.batches_epoch * (self.epoch - 1) + self.batch
+        batches_left = self.batches_epoch * (self.n_epochs - self.epoch) + self.batches_epoch - self.batch
+        sys.stdout.write('ETA: %s' % (datetime.timedelta(seconds=batches_left * self.mean_period / batches_done)))
+
+        # Draw images
+        for image_name, tensor in images.items():
+            if image_name not in self.image_windows:
+                self.image_windows[image_name] = self.viz.image(tensor2image(tensor.data), opts={'title': image_name})
+            else:
+                self.viz.image(tensor2image(tensor.data), win=self.image_windows[image_name],
+                               opts={'title': image_name})
+
+        # End of epoch
+        if (self.batch % self.batches_epoch) == 0:
+            # Plot losses
+            for loss_name, loss in self.losses.items():
+                if loss_name not in self.loss_windows:
+                    self.loss_windows[loss_name] = self.viz.line(X=np.array([self.epoch]),
+                                                                 Y=np.array([loss / self.batch]),
+                                                                 opts={'xlabel': 'epochs', 'ylabel': loss_name,
+                                                                       'title': loss_name})
+                else:
+                    self.viz.line(X=np.array([self.epoch]), Y=np.array([loss / self.batch]),
+                                  win=self.loss_windows[loss_name], update='append')
+                # Reset losses for next epoch
+                self.losses[loss_name] = 0.0
+
+            self.epoch += 1
+            self.batch = 1
+            sys.stdout.write('\n')
+        else:
+            self.batch += 1
+
+
+class ReplayBuffer():
+    def __init__(self, max_size=50):
+        assert (max_size > 0), 'Empty buffer or trying to create a black hole. Be careful.'
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0, 1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
+
+
+class LambdaLR():
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert ((n_epochs - decay_start_epoch) > 0), "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant(m.bias.data, 0.0)
+
+
---
+++ Scripts/GAN_prep.py
@@ -0,0 +1,74 @@
+# Cycle-GAN 3D preparation
+import glob
+import random
+import os
+import numpy as np
+import skimage.io as io
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+
+def construct(root):
+    imlist = random.sample(glob.glob(root + '/*/'), 9)
+    ct = 1
+    imcombined = None
+    lbcombined = None
+    for i in imlist:
+        im = io.imread(i+'image.tif')
+        lb = io.imread(i+'label.tif')
+        if ct == 1:
+            imcombined = im
+            lbcombined = lb
+        else:
+            imcombined = np.concatenate((imcombined, im), axis=1)
+            lbcombined = np.concatenate((lbcombined, lb), axis=1)
+        ct += 1
+    ic = np.concatenate((imcombined[:, :3072, :], imcombined[:, 3072:6144, :], imcombined[:, 6144:, :]), axis=2)
+    il = np.concatenate((lbcombined[:, :3072, :], lbcombined[:, 3072:6144, :], lbcombined[:, 6144:, :]), axis=2)
+
+    return ic, il
+
+
+def sampling(img, lb, bt, dir, rand_num=100):
+    # original images
+    for i in range(3):
+        for j in range(3):
+            cutim = img[:, 1024*i:1024*(i+1), 1024*j:1024*(j+1)]
+            cutlb = lb[:, 1024*i:1024*(i+1), 1024*j:1024*(j+1)]
+            io.imsave(dir+'/{}_{}_{}.tif'.format(bt, i*1024, j*1024), np.asarray(cutim).astype(np.uint8))
+            io.imsave(dir + '/{}_{}_{}_lb.tif'.format(bt, i*1024, j*1024), np.asarray(cutlb).astype(np.uint8))
+    # random
+    for m in range(rand_num):
+        x = random.randint(0, 2048)
+        y = random.randint(0, 2048)
+        cutim = img[:, x:x+1024, y:y+1024]
+        cutlb = lb[:, x:x+1024, y:y+1024]
+        io.imsave(dir + '/{}_{}_{}.tif'.format(bt, x, y), np.asarray(cutim).astype(np.uint8))
+        io.imsave(dir + '/{}_{}_{}_lb.tif'.format(bt, x, y), np.asarray(cutlb).astype(np.uint8))
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+        self.transform = transforms.Compose(transforms_)
+        self.unaligned = unaligned
+
+        self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+
+        if self.unaligned:
+            item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
+        else:
+            item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
+
+        return {'A': item_A, 'B': item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
+
+image, label = construct('../train3D')
+sampling(image, label, 1, '../random_cut')
