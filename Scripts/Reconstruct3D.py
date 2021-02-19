fd16f5ab13d58f08f222dd1a27a0cfcc47e26976
rhong3 <michaelhong1995@gmail.com>
2021-02-19 12:57:57 -0500

    2.19.3

--- Scripts/Reconstruct3D.py
+++ Scripts/Reconstruct3D.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import pandas as pd
 
+oldpd = pd.read_csv('../train3D/samples.csv', header=0)
 pdls = []
 try:
     os.mkdir('../train3D')
@@ -16,25 +17,26 @@ for i in range(1, 24):
     lb = []
     gp = []
     for j in range(1, 8):
-        image = io.imread("../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/images/C1-FB323A_CSC_Rd1_{}_{}_pad.png".format(i,j,i,j))
+        image = io.imread("../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/images/5000C1-FB323A_CSC_Rd1_{}_{}_pad.png".format(i,j,i,j))
         im.append(image[:, :, 0])
-        label = io.imread("../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/label/Combined_pad.png".format(i,j))
+        label = io.imread("../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/Combined_pad.png".format(i,j))
         lb.append(label)
-        gap = io.imread("../stage_1_train/C1-FB323A_CSC_Rd1_{}_{}/label/Gap_pad.png".format(i,j))
+        gap = io.imread("../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/Gap_pad.png".format(i,j))
         gp.append(gap)
     try:
-        os.mkdir("../train3D/C1-FB323A_CSC_Rd1_{}".format(i))
+        os.mkdir("../train3D/5000C1-FB323A_CSC_Rd1_{}".format(i))
     except FileExistsError:
         pass
-    io.imsave("../train3D/C1-FB323A_CSC_Rd1_{}/image.tif".format(i), np.asarray(im).astype(np.uint8))
-    io.imsave("../train3D/C1-FB323A_CSC_Rd1_{}/label.tif".format(i), np.asarray(lb).astype(np.uint8))
-    io.imsave("../train3D/C1-FB323A_CSC_Rd1_{}/gap.tif".format(i), np.asarray(gp).astype(np.uint8))
-    pdls.append(['fluorescence', '../inputs/train3D/C1-FB323A_CSC_Rd1_{}/image.tif'.format(i),
-                 '../inputs/train3D/C1-FB323A_CSC_Rd1_{}/label.tif'.format(i),
-                 '../inputs/train3D/C1-FB323A_CSC_Rd1_{}/gap.tif'.format(i),
-                 1024, 1024, 7, 'C1-FB323A_CSC_Rd1_{}'.format(i)])
+    io.imsave("../train3D/5000C1-FB323A_CSC_Rd1_{}/image.tif".format(i), np.asarray(im).astype(np.uint8))
+    io.imsave("../train3D/5000C1-FB323A_CSC_Rd1_{}/label.tif".format(i), np.asarray(lb).astype(np.uint8))
+    io.imsave("../train3D/5000C1-FB323A_CSC_Rd1_{}/gap.tif".format(i), np.asarray(gp).astype(np.uint8))
+    pdls.append(['fluorescence', '../inputs/train3D/5000C1-FB323A_CSC_Rd1_{}/image.tif'.format(i),
+                 '../inputs/train3D/5000C1-FB323A_CSC_Rd1_{}/label.tif'.format(i),
+                 '../inputs/train3D/5000C1-FB323A_CSC_Rd1_{}/gap.tif'.format(i),
+                 1024, 1024, 7, '5000C1-FB323A_CSC_Rd1_{}'.format(i)])
 
 pdsum = pd.DataFrame(pdls, columns=['Type', 'Image', 'Label', 'Gap', 'Width', 'Height', 'Depth', 'ID'])
+pdsum = pdsum.append(oldpd)
 pdsum.to_csv('../train3D/samples.csv', index=False)
 
 
--- Scripts/split_prep.py
+++ Scripts/split_prep.py
@@ -16,7 +16,7 @@ for i in range(1, 24):
     else:
         ptt = str(i)
     try:
-        image = io.imread("../3000C1-FB323A_CSC_Rd1 #{}.tif".format(ptt))
+        image = io.imread("../5000C1-FB323A_CSC_Rd1 #{}.tif".format(ptt))
         label = io.imread("../ManualNuclei_C2-FB323A_CSC_Rd1 #{}.tif".format(ptt))
     except FileNotFoundError:
         continue
@@ -24,15 +24,15 @@ for i in range(1, 24):
         label = (label[:, :, :, 0] / 256).astype('uint8')
     for j in range(np.shape(image)[0]):
         try:
-            os.mkdir('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}'.format(str(i), str(j+1)))
+            os.mkdir('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}'.format(str(i), str(j+1)))
         except FileExistsError:
             pass
         try:
-            os.mkdir('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/images'.format(str(i), str(j+1)))
+            os.mkdir('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/images'.format(str(i), str(j+1)))
         except FileExistsError:
             pass
         try:
-            os.mkdir('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/label'.format(str(i), str(j+1)))
+            os.mkdir('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label'.format(str(i), str(j+1)))
         except FileExistsError:
             pass
 
@@ -41,26 +41,26 @@ for i in range(1, 24):
         img[:, :, 1] = image[j, :, :]
         img[:, :, 2] = image[j, :, :]
 
-        io.imsave('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/images/3000C1-FB323A_CSC_Rd1_{}_{}_pad.png'.
+        io.imsave('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/images/5000C1-FB323A_CSC_Rd1_{}_{}_pad.png'.
                   format(str(i), str(j+1), str(i), str(j+1)), img)
-        io.imsave('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/label/Combined_pad.png'.
+        io.imsave('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/Combined_pad.png'.
                   format(str(i), str(j+1), str(i), str(j+1)), label[j, :, :])
 
         selem = mph.disk(2)
         erlabel = mph.erosion(label[j, :, :], selem)
-        io.imsave('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/label/ER_Combined_pad.png'.
+        io.imsave('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/ER_Combined_pad.png'.
                   format(str(i), str(j + 1), str(i), str(j + 1)), erlabel)
 
         selem = mph.disk(2)
         dllabel = mph.dilation(label[j, :, :], selem)
-        io.imsave('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/label/DL_Combined.png'.
+        io.imsave('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/DL_Combined.png'.
                   format(str(i), str(j + 1), str(i), str(j + 1)), dllabel)
 
         gap = dllabel - erlabel
-        io.imsave('../stage_1_train/3000C1-FB323A_CSC_Rd1_{}_{}/label/Gap_pad.png'.
+        io.imsave('../stage_1_train/5000C1-FB323A_CSC_Rd1_{}_{}/label/Gap_pad.png'.
                   format(str(i), str(j + 1), str(i), str(j + 1)), gap)
 
-        new.append(['3000C1-FB323A_CSC_Rd1_{}_{}'.format(str(i), str(j + 1)), 1024, 1024, 0, 0, 0])
+        new.append(['5000C1-FB323A_CSC_Rd1_{}_{}'.format(str(i), str(j + 1)), 1024, 1024, 0, 0, 0])
 
 
 # newpd = pd.DataFrame(new, columns=["image_id", "width", "height", "total_masks", "hsv_dominant", "hsv_cluster"])
